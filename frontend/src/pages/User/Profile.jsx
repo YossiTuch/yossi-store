83a20dd868ce598faa1742dcdb706c7800ccc242
Yossi <yossi6435@gmail.com>
2025-11-10 11:54:22 +0200

    Implemented user profile management by adding profile mutation to the API slice and integrating state management in the Profile component. Initialized form fields with user data and set up loading state for profile updates.

--- frontend/src/pages/User/Profile.jsx
+++ frontend/src/pages/User/Profile.jsx
@@ -1,4 +1,26 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import Loader from "../../components/Loader";
+import { setCredentials } from "../../redux/features/auth/authSlice";
+import { Link } from "react-router";
+import { useProfileMutation } from "../../redux/api/usersApiSlice";
+
 const Profile = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const [updateProfile, { isLoading: loadingUpdateProfile }] =
+    useProfileMutation();
+
+  useEffect(() => {
+    setUsername(userInfo.username);
+    setEmail(userInfo.email);
+  }, [userInfo.username, userInfo.email]);
   return <div>Profile</div>;
 };
 export default Profile;
